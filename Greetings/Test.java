1a978d70e91bdc993a31b9d0a5d5b2fbddb9718b
Ivan-Lpz <110691840+Ivan-Lpz@users.noreply.github.com>
2022-10-03 08:59:14 -0700

    Greetings Java

---
+++ Greetings/Test.java
@@ -0,0 +1,9 @@
+// File name must be the same as the class name 
+
+
+public class Test {
+    public static void main(String[] args) {
+        // For now, all your code goes here!
+        System.out.println("Hello World!");
+    }
+}
