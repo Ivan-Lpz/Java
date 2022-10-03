cfaa4e76ad96d2f6477e76f3b993fdf423ebbbca
Ivan-Lpz <110691840+Ivan-Lpz@users.noreply.github.com>
2022-10-03 08:57:10 -0700

    Add files via upload
    First Java assignment

---
+++ First_Java/First.java
@@ -0,0 +1,29 @@
+// public class First {
+//     public static void main(String[] args) {
+//         System.out.println("I am 100 years old");
+//     }
+// }
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("My name is Coding Dojo");
+        String firstName = "Ivan";
+        System.out.println("My name is " + firstName);
+        //System.out.println("Hello World");
+        // ---Variables ---
+        // primitive 
+        int x = 100;
+        System.out.println("I am " + x + " years old" );
+        System.out.println("My hometown is Burbank, CA");
+        // System.out.println(x);
+        boolean vote = true;
+        double tax = 3.4;
+        float xx = 3.14f;
+        char letter = 'a';
+
+        // Object Types (Class wrappers)
+        Integer y = 123123;
+        String name = "Bob";
+        Boolean isAdmin = true;
+    }
+}
